4c0a0c36a984fbb6cb6c5d3825cefbd033d60b68
AtreusV <103095292+AtreusV@users.noreply.github.com>
2025-05-12 17:25:41 -0500

    Spring Boot

---
+++ ApiR/ApiR/src/main/java/com/eDT_SHIRT/ApiR/ApiRApplication.java
@@ -0,0 +1,13 @@
+package com.eDT_SHIRT.ApiR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRApplication.class, args);
+	}
+
+}
---
+++ ApiR/ApiR/src/test/java/com/eDT_SHIRT/ApiR/ApiRApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eDT_SHIRT.ApiR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
